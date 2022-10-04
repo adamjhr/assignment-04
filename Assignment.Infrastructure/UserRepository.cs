fddcead3a0a31b91064d35b5a39146046230a54d
Silas Arildsen <43112458+silasarildsen1@users.noreply.github.com>
2022-10-04 13:19:04 +0200

    Silas (#1)
    * Implement User.Find
    
    Co-authored-by: siar <siar@itu.dk>

--- Assignment.Infrastructure/UserRepository.cs
+++ Assignment.Infrastructure/UserRepository.cs
@@ -1,5 +1,41 @@
 namespace Assignment.Infrastructure;
 
-public class UserRepository
+public class UserRepository : IUserRepository
 {
+    private KanbanContext _context;
+    public UserRepository(KanbanContext context)
+    {
+        _context = context;
+    }
+    public (Response Response, int UserId) Create(UserCreateDTO user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Response Delete(int userId, bool force = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UserDTO Find(int userId)
+    {
+        foreach (var user in _context.Users)
+        {
+            if (user.Id == userId)
+            {
+                return new UserDTO(user.Id, user.Name, user.Email);
+            }
+        }
+        return null!;
+    }
+
+    public IReadOnlyCollection<UserDTO> Read()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Response Update(UserUpdateDTO user)
+    {
+        throw new NotImplementedException();
+    }
 }
